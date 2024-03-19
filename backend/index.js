26009379bc155eab90d506f4f007c7c4037bec02
Avantika Tripathi <tripathiavantika127@gmail.com>
2024-03-19 13:09:45 +0530

    this is an ecommerce webiste
    e commerce

---
+++ backend/index.js
@@ -0,0 +1,273 @@
+const port= 4000;
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const jwt=require("jsonwebtoken");
+const multer=require("multer");
+const path=require("path");
+const cors=require("cors");
+const { request } = require("http");
+const { rmSync } = require("fs");
+
+app.use(express.json());
+app.use(cors());
+
+// DATABASE Connection with MongoDB
+mongoose.connect("mongodb+srv://Avantika:Avantika%40121@cluster0.umxzvgv.mongodb.net/e-commerce")
+
+//API CREATION
+
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+//IMAGE STORAGE ENGINE
+
+const storage= multer.diskStorage({
+    destination: './upload/images',
+    filename: (req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload= multer({storage:storage})
+
+//CREATING UPLOAD ENDTIME
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req, res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+//SCHEMA FOR CREATING PRODUCTS
+
+const Product=mongoose.model("Product", {
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required: true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type: Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    }
+})
+
+app.post('/addproduct',async (req,res)=>{
+    let products=await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array=products.slice(-1);
+        let last_product=last_product_array[0];
+        id=last_product.id+1;
+    }
+    else{
+        id=1;
+    }
+    //product schema
+    const product=new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+//creating api for getting all products
+app.get('/allproducts',async(req,res)=>{
+    let products=await Product.find({});
+    console.log("All Products Fetched")
+    res.send(products);
+})
+
+//SCHEMA CREATING FOR USER MODEL
+const Users=mongoose.model('Users',{
+    name:{
+        type:String
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type: String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type: Date,
+        default:Date.now,
+    }
+})
+
+//CREATING END POINT FOR REGISTERING THE USER
+app.post('/signup', async(req,res)=>{
+
+    let check=await Users.findOne({email:req.body.email});
+    if(check){    return res.status(400).json({success:false,errors:"existing user found with same email id"});
+    }
+    let cart={};
+    for(let i=0;i<300;i++){
+        cart[i]=0;
+    }
+    const user=new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data={
+        user:{
+            id:user.id
+        }
+    } //we are making an obbject and inside the object we are making a key to store user data in the database
+
+    const token=jwt.sign(data,'secret_ecom');  //leearn more about jwt and how we signed
+    res.json({success:true, token})
+})
+
+
+
+//creating api for deleting products
+
+app.post('/removeproduct', async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+//creating end point for user login
+app.post('/login',async(req,res)=>{
+    let user=await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare=req.body.password===user.password;
+        if(passCompare){
+            const data={
+                user:{
+                    id:user.id
+                }
+            }
+            const token=jwt.sign(data,'secret_ecom');
+            res.json({success:true,token})
+        }
+        else{
+            res.json({success:false, errors:"Wrong Password"});
+        }
+    }
+    else{
+        res.json({success:false, errors:"Wrong email-id"});
+    }
+})
+
+//CREATING END POINT FOR NEW COLLECTION DATA
+app.get('/newcollections', async(req,res)=>{
+    let products=await Product.find({});
+    let newcollection=products.slice(1).slice(-8);
+    console.log("NewCollection Fetched");
+    res.send(newcollection);
+})
+
+//CREATING END POINT FOR POPULAR IN WOMEEN SECTION
+app.get('/popularinwomen',async(req,res)=>{
+    let products=await Product.find({category:"women"});
+    let popular_in_women=products.slice(0,4);
+    console.log("Popular in Women Fetched");
+    res.send(popular_in_women);
+})
+
+//creating middleware to fetch user
+    const fetchUser=async(req,res,next)=>{
+        const token=req.header('auth-token');
+        if(!token){
+            res.status(401).send({errors:"Please authenticate using valid token"})
+        }
+        else{
+            try{
+                const data=jwt.verify(token,'secret_ecom'); //salt
+                req.user=data.user;
+                next();
+            } catch(error){
+                res.status(401).send({errors:"Please authenticate using a valid token"})
+            }
+        }
+    }
+
+
+//CREATING ENDPOINT FOR ADDING PRODUCT FOR CART DATA
+
+app.post('/addtocart',fetchUser,async(req,res)=>{
+    console.log("added", req.body.itemId);
+    let userData=await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] +=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Added")
+})
+
+//CREATING END POINT TO REMOVE PRODUCT FROM CART DATA
+app.post('/removefromcart',fetchUser,async(req,res)=>{
+    console.log("removed", req.body.itemId);
+    let userData=await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] -=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Removed")
+})
+
+//creating endppoint to get cart data
+
+app.post('/getcart',fetchUser,async(req,res)=>{
+    console.log("GetCart");
+    let userData=await Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server Running on Port "+port)
+    }
+    else{
+        console.log("Error: "+error)
+    }
+})
---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './DescriptionBox.css'
+const DescriptionBox=() =>{
+    return(
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (100)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Experience seamless shopping at our e-commerce platform, where convenience meets quality. Discover a vast array of products, from trendy fashion to cutting-edge gadgets, all at your fingertips. Enjoy secure transactions, fast delivery, and personalized recommendations. Elevate your online shopping experience with us today!</p>
+                <p>Dive into the vibrant world of CartoonMart, where imagination knows no bounds. Discover a treasure trove of merchandise inspired by your favorite cartoons and characters. From collectibles to clothing, toys to tech, find everything you need to infuse a touch of whimsy into your life. With seamless browsing, secure transactions, and speedy delivery, let CartoonMart bring your childhood dreams to life.</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
